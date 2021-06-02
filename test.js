1446c3099f62b479642a3d38d21e6d6c618d2360
CH3IRAT <83812428+CH3IRAT@users.noreply.github.com>
2021-06-02 08:50:13 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,88 @@
+var bt1 = document.getElementById("plus1")
+var count1 =document.getElementById("counter1")
+bt1.addEventListener('click',function(){
+count1.value++})
+
+var bt1 = document.getElementById("moin1")
+var count1 =document.getElementById("counter1")
+bt1.addEventListener('click',function(){
+count1.value--})
+
+
+var bt2 = document.getElementById("plus2")
+var count2 =document.getElementById("counter2")
+bt2.addEventListener('click',function(){
+count2.value++})
+
+var bt2 = document.getElementById("moin2")
+var count2 =document.getElementById("counter2")
+bt2.addEventListener('click',function(){
+count2.value--})
+
+
+var bt3 = document.getElementById("plus3")
+var count3 =document.getElementById("counter3") 
+bt3.addEventListener('click',function(){
+count3.value++})
+
+var bt3 = document.getElementById("moin3")
+var count3 =document.getElementById("counter3")
+bt3.addEventListener('click',function(){
+count3.value--})
+
+
+var galb=document.getElementById("galb")
+function myFunction(){
+
+	  galb.style.color ="red"
+
+}
+
+
+var galb2=document.getElementById("galb2")
+function myFunction2(){
+
+	  galb2.style.color ="red"
+
+}
+
+
+var galb3=document.getElementById("galb3")
+function myFunction3(){
+
+	  galb3.style.color ="red"
+
+}
+
+
+
+var faskh=document.getElementById("delete")
+function del(){
+
+	faskh.remove();
+}
+
+
+
+var faskh2=document.getElementById("delete2")
+function del2(){
+
+	faskh2.remove();
+}
+
+
+
+var faskh3=document.getElementById("delete3")
+function del3(){
+
+	faskh3.remove();
+}
+
+var q1=Number(document.getElementById("quantity").value);
+
+
+
+function somme(){
+
+ pf=iphone*p1;
+}
